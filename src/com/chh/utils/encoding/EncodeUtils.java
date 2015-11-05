1da859ccc0f5f88ee0550ab9d44ad093a60ac648
longredhao <longredhao@163.com>
2015-11-05 14:00:13 +0800

    2015年 11月 05日 星期四 14:00:13 CST commit

--- src/com/chh/utils/encoding/EncodeUtils.java
+++ src/com/chh/utils/encoding/EncodeUtils.java
@@ -459,8 +459,6 @@ public class EncodeUtils {
 		        int number = bytes[0] & 0xFF;  
 		        // "|="按位或赋值。  
 		        number |= ((bytes[1] << 8) & 0xFF00);  
-		        number |= ((bytes[2] << 16) & 0xFF0000);  
-		        number |= ((bytes[3] << 24) & 0xFF000000);  
 		        return number;  
 		    }
 		    
