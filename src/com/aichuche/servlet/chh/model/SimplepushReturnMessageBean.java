d4ef604ed7dd2daf21ba3f189aa90f613ad7a597
longredhao <longredhao@163.com>
2015-11-16 17:40:05 +0800

    2015年 11月 16日 星期一 17:40:05 CST commit

---
+++ src/com/aichuche/servlet/chh/model/SimplepushReturnMessageBean.java
@@ -0,0 +1,28 @@
+package com.aichuche.servlet.chh.model;
+
+public class SimplepushReturnMessageBean {
+	private String return_code;
+	private String return_message;
+	private String data;
+	
+	public String getReturn_code() {
+		return return_code;
+	}
+	public void setReturn_code(String return_code) {
+		this.return_code = return_code;
+	}
+	public String getReturn_message() {
+		return return_message;
+	}
+	public void setReturn_message(String return_message) {
+		this.return_message = return_message;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+
+}
