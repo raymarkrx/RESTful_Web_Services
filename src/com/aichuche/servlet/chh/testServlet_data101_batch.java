4f85181e2aed84d00dfc67538fbee1a55056b270
longredhao <longredhao@163.com>
2015-11-24 18:40:06 +0800

    2015年 11月 24日 星期二 18:40:06 CST commit

--- src/com/aichuche/servlet/chh/testServlet_data101_batch.java
+++ src/com/aichuche/servlet/chh/testServlet_data101_batch.java
@@ -54,8 +54,8 @@ public class testServlet_data101_batch {
         	//String deviceId     =i%2==0?"+86test_1":"+86test_2";
         	//String deviceId  ="test8618616969935"+rand.nextInt(99); 352273017386360 
         	
-        	String deviceId  =DEFAULT_TEST;//deviceId字段
-        	//String deviceId  ="chhtest";//deviceId字段
+        	//String deviceId  =DEFAULT_TEST;//deviceId字段
+        	String deviceId  ="352273017386360";//deviceId字段
  			String messageId="0";//递增的序列
  			String dataType="2";//表示二进制的加密后的数据
  			String data;//这个字段由下面的字段构成
